2575084fcd211188ad6383ed01c78c1c2b0ccab1
Łukasz Czyż <lukasz@lukaszczyz.com>
2023-08-18 01:34:41 +0200

    Added fake video processing implementation

---
+++ video_processing.py
@@ -0,0 +1,9 @@
+import logging
+from time import sleep
+
+
+def process_video(video_path: str):
+    """Mock function for testing purposes"""
+    logging.info(f"Processing video {video_path}")
+    sleep(3)
+    logging.info(f"Finished processing video {video_path}")
