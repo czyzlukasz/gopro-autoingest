92764d5d1ea31ee8cc780c5e1061d056ebc8ddc6
Łukasz Czyż <lukasz@lukaszczyz.com>
2023-08-18 18:50:50 +0200

    Logging level is set based on a value from config

--- gopro_ingest.py
+++ gopro_ingest.py
@@ -1,10 +1,11 @@
 import logging
 
+import ingest_config
 from http_connector import HttpClient
 from video_ingest import VideoIngester
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=ingest_config.LOGGING_LEVEL)
 
     client = HttpClient()
     video_info = client.get_video_info()
