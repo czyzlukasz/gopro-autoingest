4e0946097cba9c159305ecca85edebdb4bce45e2
Łukasz Czyż <lukasz@lukaszczyz.com>
2023-08-19 20:37:36 +0200

    Adjusted timeout values

--- ingest_config.py
+++ ingest_config.py
@@ -2,8 +2,9 @@ from os import getenv
 
 GOPRO_IP: str = getenv("GOPRO_IP", "10.5.5.9")
 GOPRO_PORT: int = int(getenv("GOPRO_PORT", 8080))
-LOWEST_DOWNLOAD_SPEED_MBYTES_PER_SECOND: float = int(getenv("LOWEST_DOWNLOAD_SPEED", 10))
+LOWEST_DOWNLOAD_SPEED_MBYTES_PER_SECOND: float = int(getenv("LOWEST_DOWNLOAD_SPEED", 5))
 
 LOGGING_LEVEL: str = getenv("LOGGING_LEVEL", "INFO")
 
 DOWNLOAD_DIR: str = getenv("DOWNLOAD_DIR", "./tmp")
+STAGING_DIR: str = getenv("STAGING_DIR", "./tmp/staging")
