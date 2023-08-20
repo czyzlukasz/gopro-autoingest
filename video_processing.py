62e4f046363a3f4aa962edfe3513d94ef6901235
Łukasz Czyż <lukasz@lukaszczyz.com>
2023-08-20 21:26:18 +0200

    Switched video processing to direct ffmpeg usage instead of ffmpeg-python library

--- video_processing.py
+++ video_processing.py
@@ -1,15 +1,37 @@
 import logging
 import os.path
+import subprocess
+import tempfile
 from os import makedirs
 
-import ffmpeg
-from typing import List
+from typing import List, IO
 
 import ingest_config
-from file_management import VideoInfo, ChapterInfo
+from file_management import VideoInfo
 
 
-def get_output_file_name(video: VideoInfo, suffix=""):
+def get_input_file_names(video: VideoInfo):
+    """
+    Create list of absolute paths to video's chapters
+    :param video: Info of the video being processed
+    :return: List of absolute paths
+    """
+    return [f"{ingest_config.STAGING_DIR}/{video.video_number}/{chapter.file_name}" for chapter in video.chapters]
+
+
+def prepare_input_file(input_files: List[str]) -> IO:
+    """
+    Create special file with instructions for the ffmpeg's concat
+    :param input_files: List of absolute paths to videos to concatenate
+    :return: File handle with instructions
+    """
+    temp_file = open("input_data.txt", "w+")
+    temp_file.writelines([f"file \'{input_path}\'\n" for input_path in input_files])
+    temp_file.close()
+    return temp_file
+
+
+def get_output_file_name(video: VideoInfo, suffix="") -> str:
     """
     Prepare path to the final output video.
     Note that in case of the file with the same name existing a suffix will be added.
@@ -24,17 +46,16 @@ def get_output_file_name(video: VideoInfo, suffix=""):
     return file_name
 
 
-def concatenate_chapters(download_path: str, chapters: List[ChapterInfo]):
+def prepare_ffmpeg_spell(input_file_name: str, output_file: str) -> str:
     """
-    Create a stream consisting of chapters.
-    Note that the stream will be in the same sequence that chapters in 'chapters' list
-    :param download_path: Directory where all chapters are located
-    :param chapters: List of chapters to concatenate
-    :return: Concatenated stream
+    Generate ready-to-execute spell
+    :param input_file_name: File containing video file paths to concat
+    :param output_file: Path where final video will be stored
+    :return: Final spell
     """
-    raise NotImplementedError()
-    chapter_file_list = [ffmpeg.input(f"{download_path}/{chapter.file_name}") for chapter in chapters]
-    return ffmpeg.concat(*chapter_file_list)
+    return f"ffmpeg -safe 0 -f concat -i {input_file_name} " \
+           f"-map 0:v -map 0:a -map 0:3 -copy_unknown -tag:2 gpmd -c copy " \
+           f"\"{output_file}\""
 
 
 def process_video(download_path: str, video_info: VideoInfo) -> bool:
@@ -46,19 +67,23 @@ def process_video(download_path: str, video_info: VideoInfo) -> bool:
     """
     logger = logging.getLogger()
 
+    input_file_names = get_input_file_names(video_info)
     output_file_name = get_output_file_name(video_info)
-    # Prepare output directory to make ffmpeg happy
-    makedirs(ingest_config.STORAGE_DIR, exist_ok=True)
+
     logger.info(f"Processing video {video_info.video_number} to {output_file_name}")
 
+    # Prepare output directory to make ffmpeg happy
+    makedirs(ingest_config.STORAGE_DIR, exist_ok=True)
+
+    # ffmpeg's concat can only read from file thus special file is needed
+    # TODO: Remove that file
+    input_file = prepare_input_file(input_file_names)
+    ffmpeg_spell = prepare_ffmpeg_spell(input_file.name, output_file_name)
+    logger.debug(f"Prepared ffmpeg spell: {ffmpeg_spell}")
     try:
-        chapter_file_list = [ffmpeg.input(f"{download_path}/{chapter.file_name}") for chapter in video_info.chapters]
-        output_stream = ffmpeg.output(*chapter_file_list, output_file_name, vcodec='copy', acodec='copy')
-        # Force overwriting destination file. This should not happen, but it prevents from being stuck indefinitely
-        output_stream = output_stream.global_args("-y")
-        output_stream.run()
-    except ffmpeg.Error as exception:
-        logger.error(f"Failed to process video: {type(exception)} {exception}")
+        subprocess.run(ffmpeg_spell, check=True)
+    except subprocess.CalledProcessError as exception:
+        logger.error(f"Failed to process video (generic exception): {type(exception)} {exception}")
         return False
     else:
         logger.info(f"Finished processing video {video_info.video_number}")
